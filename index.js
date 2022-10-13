6aa62745bf5180196a29ed4d03f23fa6763b9227
Richmond Nkansah <richmondnkansah@gmail.com>
2022-10-13 16:24:38 -0400

    commit tree 24db66fe296eea5536f693351dab69c660ff84fc

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = driversArray => driversArray.slice(0, 2);
+
+const returnLastTwoDrivers = driversArray => driversArray.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(intValue) {
+    return fareForRide => fareForRide * intValue;
+}
+
+const functionReturnedFareMultiplier  = createFareMultiplier(anyValue);
+function fareDoubler(functionReturnedFareMultiplier){
+    return functionReturnedFareMultiplier * 2;
+}
+
+function fareTripler(functionReturnedFareMultiplier){
+    return functionReturnedFareMultiplier * 3;
+}
+
+
+function selectDifferentDrivers(arrayNamesOfDrivers, driverFunctions) {
+    if (driverFunctions === returnFirstTwoDrivers) {
+        return arrayNamesOfDrivers.slice(0, 2);
+    } else {
+        return arrayNamesOfDrivers.slice(-2);
+    }
+}
